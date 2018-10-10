fd54bb9ce5a9c433cf5679cbdcc4229d27b4d242
Nikos Ntaf <nik.ntf1@gmail.com>
2018-10-10 22:37:48 +0300

    html output,test

--- Vanilla-Weather-App/js/main.js
+++ Vanilla-Weather-App/js/main.js
@@ -1,6 +1,8 @@
 function getWeather(woeid) {
 const targetUrl = `https://www.metaweather.com/api/location/${woeid}/`;
 const proxieUrl = 'https://cors-anywhere.herokuapp.com/';
+const weatherHtml = document.querySelector('.London');
+
 
 
 fetch(proxieUrl + targetUrl)
@@ -9,13 +11,29 @@ fetch(proxieUrl + targetUrl)
     return result.json();
 })
 .then(data => {
+    //test 
     console.log(data)
     const today = data.consolidated_weather[0];
     const parent = data.parent;
     //test
     console.log(`City: ${data.title} Country:  ${parent.title} with min temperature ${today.min_temp} and max temperature ${today.max_temp} Celsius , Weather is ${today.weather_state_name}`);
+    weatherHtml.innerHTML = `<h1>Weather Today</h1>
+                            <ul>
+                              <li>For City: ${data.title}</li>
+                              <li>Country: ${parent.title} </li>
+                              <li>Weather is ${today.weather_state_name}</li>
+                               </li>Min Temp: ${today.min_temp}</li>
+                               <li>Max Temp: ${today.max_temp}</li>                               
+                            <ul>
+                            `
+    //Athens.innerHTML = 
 })
 .catch(error => console.log(error));
 }
 getWeather(44418);
-getWeather(946738);
+
+
+setTimeout(() => {    
+     getWeather(946738);
+},9000)
+//getWeather(946738)
