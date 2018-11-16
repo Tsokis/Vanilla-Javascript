790426815e1b7463d619b23f79dd317c72666534
Nikos Ntaf <nik.ntf1@gmail.com>
2018-11-16 23:25:38 +0200

    bug fix

--- Vanilla-Weather-App/js/main.js
+++ Vanilla-Weather-App/js/main.js
@@ -3,9 +3,8 @@ const targetUrl = `https://www.metaweather.com/api/location/${woeid}/`;
 const proxieUrl = 'https://cors-anywhere.herokuapp.com/';
 const weatherHtml = document.querySelector('.Weather');
 const forecast = document.querySelector('.Forecast');
-weatherHtml.style.display = 'none';
-forecast.style.display = 'none';
-
+let hide = document.querySelector('.container');
+hide.style.display = 'none';
 
 fetch(proxieUrl + targetUrl)
 .then(result => {
@@ -21,13 +20,12 @@ fetch(proxieUrl + targetUrl)
     const after3Days = data.consolidated_weather[3];
     const after4Days = data.consolidated_weather[4];
     const after5Days = data.consolidated_weather[5];
-    const parent = data.parent;
-    
+    const parent = data.parent;    
     let d = new Date(); 
     dd = d.toDateString();    
     //test
     console.log(`City: ${data.title} Country:  ${parent.title} with min temperature ${today.min_temp} and max temperature ${today.max_temp} Celsius , Weather is ${today.weather_state_name}`);
-    weatherHtml.style.display = 'block';
+    hide.style.display = 'block';
     let weatherFetch = `<ul class="ulStyle">
                               <li><strong>${data.title}</strong></li>
                               <li>Country: <em>${parent.title}</em> </li>
@@ -36,9 +34,7 @@ fetch(proxieUrl + targetUrl)
                                <li>Todays Minimum Temperature expected to be<br> <strong>${today.min_temp}</strong> Celsius</li>
                                <li>Todays Maximum Temperature expected to be<br> <strong>${today.max_temp}</strong> Celsius</li>
                             </ul> `;
-    weatherHtml.innerHTML += weatherFetch;
-
-    forecast.style.display = 'block';
+    weatherHtml.innerHTML += weatherFetch;    
     let respondForecast = `<ul class="ulStyle">
                                 <h3><strong>4 days forecast</strong></h3>
                                 <h4><em>${data.title}</em></h4>
@@ -56,14 +52,10 @@ fetch(proxieUrl + targetUrl)
 setTimeout(() => {
     //London Weather
     getWeather(44418);
-    setTimeout(() => {
-        //Athens Weather
-        getWeather(946738);
-    }, 3500)
-    setTimeout(() => {
-        //Newcastle Weather
-        getWeather(30079);
-    }, 4100)
+    //Athens Weather
+    getWeather(946738);    
+     //Newcastle Weather
+    getWeather(30079);    
 },1000)
 
 
