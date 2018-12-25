42fbbdc846cb9e24304a3fa58b5086922c3dbff2
Nikos Ntaf <nik.ntf1@gmail.com>
2018-12-26 01:39:59 +0200

    local storage get add delete complete

--- ProductList/index.js
+++ ProductList/index.js
@@ -13,33 +13,78 @@ class UI {
     // instatiate our object everytime we need a method
     static displayProducts(){
         //from local storage
-        const test = [{name:'hehh',price:22,description:'hahahahahahahahahhahahahahaha'}]
+        const test = Storage.getProducts();
         const tt = test;
         tt.forEach((value)=>{
             UI.addProductToList(value);
         })
     }
+    // add product static method
     static addProductToList(product) {
         const list = document.getElementById('product-list')
         const row = document.createElement('tr');
         row.innerHTML = `
-        <td>${product.name}</td>
-        <td>${product.price}</td>
-        <td>${product.description}</td>  
+        <td>${product.name}</td>        
+        <td>${product.description}</td>
+        <td>${product.price}</td>  
         <td><a class="delete">X</a></td>  
         `;
         list.appendChild(row);
     }
+    //delete product static method
     static deleteBook(el) {
         if(el.classList.contains('delete')){
             //<a>X</a> --> <td></td> --> <tr></tr>
             el.parentElement.parentElement.remove();
         }
     }
+    // show alert on empty submit
+    static showAlert(message,className){
+        // <div class="alert-alert or success">some error or success message</div>
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        container.insertBefore(div,form);
+        // gone after 2.5 seconds
+        setTimeout(() => document.querySelector('.alert').remove(),2500);
+    }
+    // clear the form fields after submit
     static clearFields(){
         document.querySelector('#name').value = '';
         document.querySelector('#price').value = '';
         document.querySelector('#desc').value = '';
+    }
+}
+
+
+// Store the data Class: handles storage
+class Storage{
+    //get Products
+    static getProducts(){
+        let products;
+        if(localStorage.getItem('products')=== null){
+            products = [];
+        }else {
+            products = JSON.parse(localStorage.getItem('products'));
+        }
+        return products;
+    }
+    //add Products
+    static addProducts(product) {
+        const products = Storage.getProducts();
+        products.push(product);
+        localStorage.setItem('products',JSON.stringify(products));
+    }
+    //remove Products
+    static removeProducts() {
+        const products = Storage.getProducts();
+        products.forEach((product,index)=> {
+            if(product.price === price){
+                product.splice(index, 1);
+            }
+        });
+        localStorage.setItem('products',JSON.stringify(products));
     }
 }
 // DOM Event : display products
@@ -52,16 +97,29 @@ document.querySelector('#form').addEventListener('submit',(e)=>{
     const name  = document.querySelector('#name').value;
     const price = document.querySelector('#price').value;
     const desc = document.querySelector('#desc').value;
-    // instatiate product
-    const product = new Product(name,price,desc);
-    // add product to UI
-    UI.addProductToList(product);
-    console.log(product);
-    // clear form fields
-    UI.clearFields();
+    // validation
+    if (name ==='' || price ==='' || desc===''){
+        UI.showAlert('Please fill all the fields','error');
+    }else{
+        // instatiate product
+        const product = new Product(name, price, desc);
+        // add product to UI method call
+        UI.addProductToList(product);
+        // add product to localstorage
+        Storage.addProducts(product);
+        console.log(product);
+        // show success message on adding a product
+        UI.showAlert('Product Added','success');
+        // clear form fields method call
+        UI.clearFields();
+        }    
 });
-
 // DOM EVENT: remove a product
 document.querySelector('#product-list').addEventListener('click', e =>{
+    // remove product ui method call
     UI.deleteBook(e.target);
+    // remove product from the localstorage method call
+    Storage.removeProducts(e.target.parentElement.previousElementSibling.textContent);
+    // show success message on adding a product
+    UI.showAlert('Book Removed', 'success');
 })
